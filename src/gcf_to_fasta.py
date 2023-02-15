ad1d7ab1368f984ebf7015d8c461672843a04e3f
Omer Acar <omeracar0@gmail.com>
2023-02-15 12:04:08 -0500

    commented out irrelevant parts

--- src/gcf_to_fasta.py
+++ src/gcf_to_fasta.py
@@ -4,49 +4,58 @@ import pandas as pd
 import subprocess
 from Bio import SeqIO
 
-df = pd.read_csv('data/interim/ybr_locus_intersect_bcftools.gvcf','\t',skiprows=61)
+#ybr_locus.bed is a bed file with the coordinates of the YBR locus
+# that spans from 612230 to 615856
+# chromosome format is chromosome2
+#1011Matrix.gvcf.gz is a gvcf file with the genotypes of 1011 yeast strains
+#Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa is the reference genome of yeast
+
+subprocess.run("bcftools view -R data/interim/ybr_locus.bed data/raw/1011Matrix.gvcf.gz > data/interim/ybr_locus_intersect_bcftools.gvcf",shell=True)
+
+df = pd.read_csv('data/interim/ybr_locus_intersect_bcftools.gvcf',sep='\t',skiprows=61)
 df = df.rename(columns={"ALT.1":"ALT"})
+subprocess.run("gatk CreateSequenceDictionary -R data/raw/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa",shell=True)
 
-subprocess.run("bcftools view -R ybr_locus.bed 1011Matrix.gvcf.gz > ybr_locus_intersect_bcftools.gvcf",shell=True)
+subprocess.run("gatk IndexFeatureFile -I data/interim/ybr_locus_intersect_bcftools.gvcf",shell=True)
 
-subprocess.run("gatk CreateSequenceDictionary -R Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa",shell=True)
+# for i in df.columns[9:]:   
+#     cmd = f"gatk SelectVariants -R data/raw/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa -V data/raw/1011Matrix.gvcf.gz --restrict-alleles-to BIALLELIC -O data/interim/biallelic.vcf"
+#     subprocess.run(cmd,shell=True)
 
-subprocess.run("gatk IndexFeatureFile -I ybr_locus_intersect_bcftools.gvcf",shell=True)
+# for i in df.columns[9:]:   
+#     cmd = f"gatk FastaAlternateReferenceMaker -R data/raw/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa -V sample_vcf/{i}.vcf  -O sample_genome/{i}.fa"
+#     subprocess.run(cmd,shell=True)
+
+# for i in df.columns[9:]:   
+#     cmd = f"bedtools getfasta -fi sample_genome/{i}.fa -bed ybr_locus.bed -fo sample_genome_ybr/{i}.fa"
+#     subprocess.run(cmd,shell=True)
+
+# files = os.listdir('sample_genome_ybr/')
+# records = [] 
+# for f in files:
+#     id_ = f.split('.')[0]
+#     for record in SeqIO.parse(f"sample_genome_ybr/{f}",'fasta'):
+#         record.id=id_
+#         records.append(record.reverse_complement())
+
+# SeqIO.write(records,"1011_ybr_rev.fasta","fasta")
+os.mkdir('data/interim/ybr_locus_consensus_per_genome')
 for i in df.columns[9:]:   
-    cmd = f"gatk SelectVariants -R data/raw/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa -V data/raw/1011Matrix.gvcf --restrict-alleles-to BIALLELIC -O data/interim/biallelic.vcf"
+    cmd = f"samtools faidx data/raw/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa chromosome2:612230-615856| bcftools consensus --sample {i} data/raw/1011Matrix.gvcf.gz > data/interim/ybr_locus_consensus_per_genome/{i}.fasta"
     subprocess.run(cmd,shell=True)
 
-for i in df.columns[9:]:   
-    cmd = f"gatk FastaAlternateReferenceMaker -R Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa -V sample_vcf/{i}.vcf  -O sample_genome/{i}.fa"
-    subprocess.run(cmd,shell=True)
-
-for i in df.columns[9:]:   
-    cmd = f"bedtools getfasta -fi sample_genome/{i}.fa -bed ybr_locus.bed -fo sample_genome_ybr/{i}.fa"
-    subprocess.run(cmd,shell=True)
-
-files = os.listdir('sample_genome_ybr/')
+files = os.listdir('data/interim/ybr_locus_consensus_per_genome/')
 records = [] 
 for f in files:
     id_ = f.split('.')[0]
-    for record in SeqIO.parse(f"sample_genome_ybr/{f}",'fasta'):
-        record.id=id_
-        records.append(record.reverse_complement())
-
-SeqIO.write(records,"1011_ybr_rev.fasta","fasta")
-
-for i in df.columns[9:]:   
-    cmd = f"samtools faidx Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa chromosome2:612230-615856| bcftools consensus --sample {i} 1011Matrix.gvcf.gz > sample_genome_ybr_consensus/{i}.fasta"
-    subprocess.run(cmd,shell=True)
-
-files = os.listdir('sample_genome_ybr_consensus/')
-records = [] 
-for f in files:
-    id_ = f.split('.')[0]
-    for record in SeqIO.parse(f"sample_genome_ybr_consensus/{f}",'fasta'):
+    # if id_ == 'ALT':
+    #     id_ = 'ALT.1'
+    for record in SeqIO.parse(f"data/interim/ybr_locus_consensus_per_genome/{f}",'fasta'):
         record = record.reverse_complement()
         record.id=id_
+        record.description=''
         records.append(record)
 
-SeqIO.write(records,"1011_ybr_rev_cons.fasta","fasta")
+SeqIO.write(records,"data/interim/1011_ybr_rev_cons.fasta","fasta")
 
 
